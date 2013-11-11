e829efc67841f8912aa1f23378109e3d04656602
Maxwell Morais <max.morais.dmm@gmail.com>
2013-11-11 10:02:34 -0200

    Small fixes in lead.js

--- selling/doctype/lead/lead.js
+++ selling/doctype/lead/lead.js
@@ -26,8 +26,6 @@ erpnext.LeadController = wn.ui.form.Controller.extend({
 			cur_frm.footer.help_area.innerHTML = '<hr>\
 				<p><a href="#Form/Sales Email Settings">'+wn._('Sales Email Settings')+'</a><br>\
 				<span class="help">'+wn._('Automatically extract Leads from a mail box e.g.')+' "sales@example.com"</span></p>';
-				<p><a href="#Form/Sales Email Settings">' + wn._('Sales Email Settings') + '</a><br>\
-				<span class="help">' + wn._('Automatically extract Leads from a mail box e.g. \"sales@example.com\"') + '</span></p>';
 		}
 	},
 	
@@ -35,7 +33,7 @@ erpnext.LeadController = wn.ui.form.Controller.extend({
 		var doc = this.frm.doc;
 		erpnext.hide_naming_series();
 		this.frm.clear_custom_buttons();
-				
+
 		this.frm.__is_customer = this.frm.__is_customer || this.frm.doc.__is_customer;
 		if(!this.frm.doc.__islocal && !this.frm.__is_customer) {
 			this.frm.add_custom_button(wn._("Create Customer"), this.create_customer);
